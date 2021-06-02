58f8b52aabe8c5525c7b62d9b86124d6ed85cb05
Hoang NGUYEN <baauco@gmail.com>
2021-06-03 00:44:08 +0700

    Renamed project

--- database_app/admin.py
+++ database_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(metatable)
+admin.site.register(branch)
+admin.site.register(setting)
+admin.site.register(role)
+admin.site.register(user)
+admin.site.register(course)
+admin.site.register(class_metadata)
+admin.site.register(class_student)
+admin.site.register(session)
+admin.site.register(attendance)
+admin.site.register(log)
