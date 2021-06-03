3ac81b97930084405ac8261805fe0f89c951b339
Hoàng <10150902+Smithienious@users.noreply.github.com>
2021-06-04 01:34:25 +0700

    Revert "Added API template"
    This reverts commit bf30d1452f114067fc7c6e0c9e0bebc64cfd773c.

--- api_gateway/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path('test', views.test, name='test-connection'),
-    path('login', views.login, name='login'),
-    # path('teacher-check', views.teacher_check, name='teacher-check')
-]
