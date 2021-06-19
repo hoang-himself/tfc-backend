cf8b195e5a8e8ea579c73589f4fccd5a8f34faa6
Hoang NGUYEN <baauco@gmail.com>
2021-06-20 00:04:44 +0700

    Added endpoints for auth/check and auth/refresh

--- app_auth/views.py
+++ app_auth/views.py
@@ -1,55 +1,44 @@
-from os import access
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
 
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
-from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
+# from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 
-from master_db.models import (
-    Metatable, Branch, Setting, Role, Course,
-    ClassMetadata, ClassStudent, ClassTeacher, Session, Attendance, Log
-)
-from master_db.serializers import (
-    MetatableSerializer, BranchSerializer, SettingSerializer, RoleSerializer, UserSerializer, CourseSerializer,
-    ClassMetadataSerializer, ClassMetadataSerializer, ClassMetadataSerializer, SessionSerializer, AttendanceSerializer, LogSerializer
-)
+from master_db.serializers import UserSerializer
 from .utils import (
     generate_access_token,
-    generate_refresh_token, get_setting_session
+    generate_refresh_token,
+    validate_refresh_token,
 )
-
-import datetime
-import jwt
 
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
-@ensure_csrf_cookie
-def login(request):
+# @ensure_csrf_cookie
+def login(request) -> Response:
     response = Response()
     response.status_code = status.HTTP_400_BAD_REQUEST
     User = get_user_model()
     valid = 1
     errors = []
 
-    email = request.POST.get('email')
+    username = request.POST.get('username')
     password = request.POST.get('password')
 
-    if (email is None or email == ''):
+    if (username is None or username == ''):
         errors.append(
             {
-                "email": "this field is required"
+                "username": "required"
             }
         )
         valid -= 1
     if (password is None or password == ''):
         errors.append(
             {
-                "password": "this field is required"
+                "password": "required"
             }
         )
         valid -= 1
@@ -60,14 +49,14 @@ def login(request):
         }
         return response
 
-    user = User.objects.filter(email=email).first()
+    user = User.objects.filter(username=username).first()
     if (user is None):
         response.data = {
             "result": "error",
             "errors": [
                 {
-                    "email": "not found"
-                },
+                    "username": "not_found"
+                }
             ]
         }
         return response
@@ -79,8 +68,8 @@ def login(request):
             "result": "error",
             "errors": [
                 {
-                    "password": "no match"
-                },
+                    "password": "no_match"
+                }
             ]
         }
         return response
@@ -89,96 +78,121 @@ def login(request):
     refresh_token = generate_refresh_token(user)
 
     response.set_cookie(key='refreshtoken', value=refresh_token, httponly=True)
-    response.status_code = status.HTTP_200_OK
+    response.status_code = status.HTTP_202_ACCEPTED
     response.data = {
         "result": "ok",
         "token": {
-            "access": access_token,
-        },
+            "access": access_token
+        }
     }
     return response
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def check(request):
-    pass
-
-
-@api_view(['DELETE'])
-@permission_classes([AllowAny])
-def logout(request):
-    pass
-
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-# @csrf_protect
-def refresh(request):
+def refresh_token_response(refresh_token):
     response = Response()
     User = get_user_model()
+    response.status_code = status.HTTP_403_FORBIDDEN
 
-    refresh_token = request.COOKIES.get('refreshtoken')
-    if (refresh_token is None):
-        response.status_code = status.HTTP_403_FORBIDDEN
-        response.data = {
-            "result": "error",
-            "errors": [
-                {
-                    "refresh token not found"
-                },
-            ]
-        }
-        return response
+    (result, payload) = validate_refresh_token(refresh_token)
 
-    try:
-        payload = jwt.decode(
-            refresh_token, settings.REFRESH_TOKEN_SECRET, algorithms=['HS256'])
-    except jwt.ExpiredSignatureError:
-        response.status_code = status.HTTP_401_UNAUTHORIZED
-        response.data = {
-            "result": "error",
-            "errors": [
-                {
-                    "refresh token expired"
-                },
-            ]
-        }
-        return response
-    except jwt.InvalidSignatureError:
-        response.status_code = status.HTTP_403_FORBIDDEN
-        response.data = {
-            "result": "error",
-            "errors": [
-                {
-                    "invalid refresh token"
-                },
-            ]
-        }
-        return response
+    if (result != 0):
+        # You want switch-case? Install python 3.10
+        if (result == -2):
+            response.status_code = status.HTTP_401_UNAUTHORIZED
+            response.data = {
+                "result": "error",
+                "errors": [
+                    "not logged in"
+                ]
+            }
+        elif (result == 2):
+            response.data = {
+                "result": "error",
+                "errors": [
+                    "expired",
+                    "refreshtoken"
+                ]
+            }
+        else:
+            response.data = {
+                "result": "error",
+                "errors": [
+                    "invalid",
+                    "refreshtoken"
+                ]
+            }
+        return (None, response)
 
-    user = User.objects.filter(username=payload.get('user_id')).first()
+    user = User.objects.filter(username=payload.get('sub')).first()
     if (user is None):
         response.status_code = status.HTTP_400_BAD_REQUEST
         response.data = {
             "result": "error",
             "errors": [
                 {
-                    "user not found"
+                    "user": "not found"
                 },
             ]
         }
-        return response
+        return (user, response)
     if not (user.is_active):
         response.status_code = status.HTTP_403_FORBIDDEN
         response.data = {
             "result": "error",
             "errors": [
                 {
-                    "user is inactive"
+                    "user": "inactive"
                 },
             ]
         }
+        return (None, response)
+    return (user, response)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def check(request) -> Response:
+    response = Response()
+    response.status_code = status.HTTP_403_FORBIDDEN
+
+    refresh_token = request.COOKIES.get('refreshtoken')
+    user, response = refresh_token_response(refresh_token)
+    if (user is None):
+        return response
+
+    roles = [
+        # TODO
+    ]
+    permissions = [
+        # TODO
+    ]
+
+    response.status_code = status.HTTP_200_OK
+    response.data = {
+        "result": "ok",
+        "isAuthenticated": True,
+        "roles": roles,
+        "permissions": permissions,
+    }
+    return response
+
+
+@api_view(['DELETE'])
+@permission_classes([AllowAny])
+def logout(request) -> Response:
+    pass
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+# @csrf_protect
+def refresh(request) -> Response:
+    response = Response()
+
+    refresh_token = request.COOKIES.get('refreshtoken')
+    token_data = refresh_token_response(refresh_token)
+    user, response = token_data
+    if (user is None):
         return response
 
     user = UserSerializer(user).data
