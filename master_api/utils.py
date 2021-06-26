64695e61930b8346d2582ef66372c6b18ed72795
Hoang NGUYEN <baauco@gmail.com>
2021-06-27 01:09:03 +0700

    Updated account endpoints

--- master_api/utils.py
+++ master_api/utils.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from rest_framework import status
+from rest_framework.response import Response
+
 from master_db.serializers import MyUserSerializer, RoleSerializer
 
 import datetime
@@ -65,32 +67,37 @@ def gen_acc_token(user):
 
 
 def has_perm(request, perms):
+    response = Response()
+    access_token = request.COOKIES.get('access_token')
 
-    authorization_header = request.headers.get('Authorization')
-
-    if not authorization_header:
-        return None
-    try:
-        # header = 'Token xxxxxxxxxxxxxxxxxxxxxxxx'
-        access_token = authorization_header.split(' ')[1]
-        payload = jwt.decode(
-            access_token, settings.SECRET_KEY, algorithms=['HS256'])
-    except jwt.ExpiredSignatureError:
-        return status.HTTP_401_UNAUTHORIZED
-    except IndexError:
-        return status.HTTP_400_BAD_REQUEST
-
-    if (access_token is None or access_token == ''):
-        return status.HTTP_400_BAD_REQUEST
+    if not access_token:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        response.data = {
+            'message': 'User not logged in'
+        }
+        return response
 
     try:
         payload = jwt.decode(
             access_token, settings.SECRET_KEY, algorithms=['HS256'])
     except jwt.ExpiredSignatureError:
-        return status.HTTP_400_BAD_REQUEST
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        response.data = {
+            'message': 'Session expired'
+        }
+        return response
 
     claimed_perms = payload['perms']
     for perm in perms:
         if perm in claimed_perms:
-            return status.HTTP_200_OK
-    return status.HTTP_401_UNAUTHORIZED
+            response.status_code = status.HTTP_200_OK
+            response.data = {
+                'message': 'Authorized'
+            }
+            return response
+
+    response.status_code = status.HTTP_401_UNAUTHORIZED
+    response.data = {
+        'message': 'Unauthorized'
+    }
+    return response
