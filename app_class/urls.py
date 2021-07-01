29d62cd4f045e0772d9c816d26c891a16d55a75c
simplynormal <nguyenchinhkhoi@gmail.com>
2021-07-01 22:31:24 +0700

    Implemented editing class function

--- app_class/urls.py
+++ app_class/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # TODO
     path('list', views.list_class, name='list_classes'),
     path('create', views.create_class, name='create'),
+    path('edit', views.edit_class, name='edit'),
     path('add-student', views.add_student, name='add-student'),
     path('delete-student', views.delete_student, name='delete-student'),
 ]
