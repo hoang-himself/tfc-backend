f377af70c2989c9c28a6ddae88ff62ed6f7ed124
simplynormal <nguyenchinhkhoi@gmail.com>
2021-07-02 11:16:43 +0700

    Fixed response after full_clean catch, fixed get course in edit_class

--- app_class/views.py
+++ app_class/views.py
@@ -18,18 +18,21 @@ import datetime
 import json
 import jwt
 
+def verify_teacher(user):
+    # In production
+    return user.mobile == '0919877216'
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def create_class(request):
     """
         Take in course_name, name, teacher_uuid (Optional), status (Need revision), std_uuids (Optional)
-        
+
         Param std_uuids must be in form of: uuid1, uuid2, uuid3 (whitespace is optional)
     """
     std_uuids = request.POST.get('std_uuids')
     teacher = request.POST.get('teacher_uuid')
-    
+
     # Get course corresponding to course_name
     try:
         course = Course.objects.get(name=request.POST.get('course_name'))
@@ -41,7 +44,7 @@ def create_class(request):
             },
             status=status.HTTP_404_NOT_FOUND
         )
-        
+
     # Get teacher if available
     if teacher is not None:
         try:
@@ -51,7 +54,7 @@ def create_class(request):
                 message = 'Teacher user does not exist'
                 stat = status.HTTP_404_NOT_FOUND
             else:
-                message = e
+                message = dict(e)
                 stat = status.HTTP_400_BAD_REQUEST
             return Response(
                 data={
@@ -61,6 +64,15 @@ def create_class(request):
                 status=stat
             )
         
+        if not verify_teacher(teacher):
+            return Response(
+                data={
+                    'details': 'Error',
+                    'message': 'User is not a teacher'
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
     # Construct model
     now = datetime.datetime.now().timestamp()
     classMeta = ClassMetadata(
@@ -71,27 +83,32 @@ def create_class(request):
         created_at=now,
         updated_at=now
     )
-    
+
     # Validate model
     try:
         classMeta.full_clean()
     except ValidationError as message:
+        print(message)
         return Response(
-            data=message,
+            data={
+                'details': 'Error',
+                'message': dict(message)
+            },
             status=status.HTTP_400_BAD_REQUEST
         )
-    
+
     # Get students from std_uuids
     uuids = None
     if not std_uuids is None:
-        # Handling UUID validation 
+        # Handling UUID validation
         try:
-            db = MyUser.objects.filter(uuid__in=std_uuids.replace(' ', '').split(','))
+            db = MyUser.objects.filter(
+                uuid__in=std_uuids.replace(' ', '').split(','))
         except ValidationError as message:
             return Response(
                 data={
                     'details': 'Error',
-                    'message': message
+                    'message': dict(message)
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
@@ -100,7 +117,7 @@ def create_class(request):
         students = db.values_list('pk', flat=True)
         # Store uuids for visualizing added students, if one does not show up it is not found
         uuids = db.values_list('uuid', flat=True)
-        
+
     # Save and add students (M2M field must be added this way to save)
     classMeta.save()
     if not std_uuids is None:
@@ -115,17 +132,18 @@ def create_class(request):
         status=status.HTTP_201_CREATED
     )
 
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def add_student(request):
     """
         Take in name, uuids. Param name represents class name and uuids is a list of student's uuid
-        
+
         Param uuids must be in the form of: uuid1, uuid2, uuid3 (whitespace is optional)
     """
     std_uuids = request.POST.get('uuids')
-    
-    # Get class 
+
+    # Get class
     try:
         classMeta = ClassMetadata.objects.get(name=request.POST.get('name'))
     except ClassMetadata.DoesNotExist:
@@ -136,19 +154,19 @@ def add_student(request):
             },
             status=status.HTTP_404_NOT_FOUND
         )
-        
+
     # Get all students with uuids
     uuids = None
     if not std_uuids is None:
         std_uuids = std_uuids.replace(' ', '').split(',')
-        # Handling UUID validation 
+        # Handling UUID validation
         try:
             db = MyUser.objects.filter(uuid__in=std_uuids)
         except ValidationError as message:
             return Response(
                 data={
                     'details': 'Error',
-                    'message': message
+                    'message': dict(message)
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
@@ -157,8 +175,8 @@ def add_student(request):
         students = db.values_list('pk', flat=True)
         # Store uuids for visualizing added students, if one does not show up it is not found
         uuids = db.values_list('uuid', flat=True)
-        
-    # Add students 
+
+    # Add students
     classMeta.students.add(*students)
 
     return Response(
@@ -168,19 +186,20 @@ def add_student(request):
         },
         status=status.HTTP_202_ACCEPTED
     )
-    
+
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def delete_student(request):
     """
         Take in name and uuids. Param name represents class name, uuids is a list of student uuid.
-        
+
         Every uuid must be a valid student or else removal will not be performed.
-        
+
         Param uuids must be in the form of: uuid1, uuid2, uuid3 (whitespace is optional)
     """
     std_uuids = request.POST.get('uuids')
-    
+
     # Get class
     try:
         classMeta = ClassMetadata.objects.get(name=request.POST.get('name'))
@@ -192,24 +211,25 @@ def delete_student(request):
             },
             status=status.HTTP_404_NOT_FOUND
         )
-        
+
     # Get students uuids
     uuids = None
     if not std_uuids is None:
         std_uuids = std_uuids.replace(' ', '').split(',')
-        # Handling UUID validation 
+        # Handling UUID validation
         try:
             students = classMeta.students.filter(uuid__in=std_uuids)
         except ValidationError as message:
             return Response(
                 data={
                     'details': 'Error',
-                    'message': message
+                    'message': dict(message)
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
         # Store uuids for visualizing added students, if one does not show up it is not found
-        uuids = [str(o) for o in students.values_list('uuid', flat=True).filter()]
+        uuids = [str(o)
+                 for o in students.values_list('uuid', flat=True).filter()]
 
         if len(uuids) != len(std_uuids):
             return Response(
@@ -219,7 +239,7 @@ def delete_student(request):
                     'not_found': set(std_uuids).difference(set(uuids))
                 }
             )
-            
+
     # Remove students from class if all uuids are present
     classMeta.students.remove(*students.values_list('pk', flat=True))
 
@@ -229,7 +249,8 @@ def delete_student(request):
         },
         status=status.HTTP_202_ACCEPTED
     )
-    
+
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def edit_class(request):
@@ -237,20 +258,21 @@ def edit_class(request):
         Take in target_name, course_name (optional), teacher (optional), course (optional), name (optional), status (optional).
 
         The optional params if not provided will not be updated. If the content provided is the same as the source, no change will be made.
-        
+
         If at least one optional param is provided, updated_at will be updated
     """
     teacher = request.POST.get('teacher_uuid')
     course = request.POST.get('course_name')
     name = request.POST.get('name')
     stat = request.POST.get('status')
-    
+
     # List represents modified fields
     modifiedList = []
-    
+
     # Get class
     try:
-        classMeta = ClassMetadata.objects.get(name=request.POST.get('target_name'))
+        classMeta = ClassMetadata.objects.get(
+            name=request.POST.get('target_name'))
     except ClassMetadata.DoesNotExist:
         return Response(
             data={
@@ -259,7 +281,7 @@ def edit_class(request):
             },
             status=status.HTTP_404_NOT_FOUND
         )
-        
+
     # Get teacher if provided
     if not teacher is None:
         try:
@@ -268,7 +290,7 @@ def edit_class(request):
             if type(e).__name__ == 'DoesNotExist':
                 message = 'Teacher user does not exist'
             else:
-                message = e
+                message = dict(e)
             return Response(
                 data={
                     'details': 'Error',
@@ -276,26 +298,29 @@ def edit_class(request):
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
-        
-    # Get course if provided
-    if not course is None:
-        try:
-            course = MyUser.objects.get(name=course)
-        except (MyUser.DoesNotExist, ValidationError) as e:
-            if type(e).__name__ == 'DoesNotExist':
-                message = 'Teacher user does not exist'
-                stat = status.HTTP_404_NOT_FOUND
-            else:
-                message = e
-                stat = status.HTTP_400_BAD_REQUEST
+            
+        if not verify_teacher(teacher):
             return Response(
                 data={
                     'details': 'Error',
-                    'message': message
+                    'message': 'User is not a teacher'
                 },
-                status=stat
+                status=status.HTTP_400_BAD_REQUEST
             )
-        
+
+    # Get course if provided
+    if not course is None:
+        try:
+            course = Course.objects.get(name=course)
+        except Course.DoesNotExist:
+            return Response(
+                data={
+                    'details': 'Error',
+                    'message': 'Course does not exist'
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
     # Update the provided fields if content changed
     if not name is None and name != classMeta.name:
         classMeta.name = name
@@ -312,7 +337,7 @@ def edit_class(request):
     if bool(modifiedList):
         classMeta.updated_at = datetime.datetime.now().timestamp()
         modifiedList.append('updated_at')
-    
+
     # Validate model
     try:
         classMeta.full_clean()
@@ -320,14 +345,14 @@ def edit_class(request):
         return Response(
             data={
                 'details': 'Error',
-                'message': message
+                'message': dict(message)
             },
             status=status.HTTP_400_BAD_REQUEST
         )
-        
+
     # Save
     classMeta.save()
-    
+
     return Response(
         data={
             'details': 'Ok',
@@ -337,30 +362,43 @@ def edit_class(request):
         },
         status=status.HTTP_202_ACCEPTED
     )
-    
+
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def delete_class(request):
-    name = request.POST.get('name')
-    
+    """
+        Take in name. Delete exactly one class with the given name.
+    """
+    # Get class
     try:
-        classMeta = ClassMetadata.objects.get(name=name)
+        classMeta = ClassMetadata.objects.get(name=request.POST.get('name'))
     except ClassMetadata.DoesNotExist:
         return Response(
             data={
                 'details': 'Error',
                 'message': 'Class does not exist'
             },
-            status=status.HTTP_400_BAD_REQUEST
+            status=status.HTTP_404_NOT_FOUND
         )
-        
+
+    # Delete
+    classMeta.delete()
+
     return Response(
-        data=ClassMetadataSerializer(classMeta).data
+        data={
+            'details': 'Ok',
+            # ! For testing purposes only, should be removed
+            'data' : ClassMetadataSerializer(classMeta).data,
+        },
+        status=status.HTTP_202_ACCEPTED
     )
-    
+
+
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def list_class(request):
     return Response(
-        data=ClassMetadataSerializer(ClassMetadata.objects.all(), many=True).data
+        data=ClassMetadataSerializer(
+            ClassMetadata.objects.all(), many=True).data
     )
--- app_course/views.py
+++ app_course/views.py
@@ -15,15 +15,17 @@ from master_db.serializers import CourseSerializer
 import datetime
 
 # Create your views here.
+
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def create_course(request):
     """
         Take in name, desc, short_desc, tags and duration
-        
+
         Param tags must be in the form of: tag1, tag2, tag3 (whitespace is optional)
     """
-    
+
     # Get fields. Should not use **request.POST because it will cause everything to be string -> invalid type
     now = datetime.datetime.now().timestamp()
     course = Course(
@@ -34,7 +36,7 @@ def create_course(request):
         created_at=now,
         updated_at=now
     )
-    
+
     # Validate model
     try:
         course.full_clean()
@@ -42,15 +44,15 @@ def create_course(request):
         return Response(
             data={
                 'details': 'Error',
-                'message': message
-            }, 
+                'message': dict(message)
+            },
             status=status.HTTP_400_BAD_REQUEST
         )
-        
+
     # Save and update tags (Tags must be done this way to be saved in the database)
     course.save()
     course.tags.add(*request.POST.get('tags').replace(' ', '').split(','))
-            
+
     return Response(
         data={
             'details': 'Ok',
@@ -59,19 +61,20 @@ def create_course(request):
         status=status.HTTP_200_OK
     )
 
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def edit_course(request):
     """
         Take in target_name, name (optional), desc (optional), short_desc (optional), tags (optional) and duration (optional). Param target_name is the name of the course that needs editing, the other params are the fields that needs changing
-        
+
         The optional params if not provided will not be updated. If the content provided is the same as the source, no change will be made.
-        
+
         Param tags must be in the form of: tag1, tag2, tag3 (whitespace is optional)
     """
-    
+
     modifiedList = []
-    
+
     # Check existence
     try:
         course = Course.objects.get(name=request.POST.get('target_name'))
@@ -83,15 +86,15 @@ def edit_course(request):
             },
             status=status.HTTP_400_BAD_REQUEST
         )
-    
+
     # Update model: Set attributes and update updated_at
     data = request.POST.copy()
-    
+
     for key, value in data.items():
         if hasattr(course, key) and value != getattr(course, key) and key != 'tags':
             setattr(course, key, value)
             modifiedList.append(key)
-    
+
     # Validate model
     try:
         course.full_clean()
@@ -99,11 +102,11 @@ def edit_course(request):
         return Response(
             data={
                 'details': 'Error',
-                'message': message
+                'message': dict(message)
             },
             status=status.HTTP_400_BAD_REQUEST
         )
-    
+
     # Save and update tags (Tags must be done this way to be saved in the database)
     tags = request.POST.get('tags')
     if not tags is None:
@@ -126,7 +129,8 @@ def edit_course(request):
         },
         status=status.HTTP_202_ACCEPTED
     )
-    
+
+
 def formdata_bool(var):
     low = var.lower()
     if low == 'true':
@@ -134,20 +138,21 @@ def formdata_bool(var):
     if low == 'false':
         return False
     return None
-    
+
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def delete_course(request):
     """
         There are 2 options.
-        
+
         Option 1: delete by name. This will only delete 1 course with the corresponding name because name is unique
-        
+
         Option 2: delete by tags. Param tags, many, exact must be specified. Param many and exact is boolean type (valid if after lowered it is 'true' or 'false'). Param many allows to delete multiple with the tags, exact allows to delete course with exact tags or not.
-        
+
         Param tags must be in the form of: tag1, tag2, tag3 (whitespace is optional)
     """
-    
+
     # Option 1: name is presence, delete 1 course by name
     name = request.POST.get('name')
     if not name is None:
@@ -166,29 +171,29 @@ def delete_course(request):
             data={'details': 'Ok'},
             status=status.HTTP_200_OK
         )
-    
+
     # Option 2: name is not presence, all other params are mandatory
     tags = request.POST.get('tags')
     many = request.POST.get('many')
     exact = request.POST.get('exact')
-    
+
     # Check for all param presence
     returnDict = {}
     if tags is None:
         returnDict.update({'tags': 'This field cannot be empty'})
     else:
         tags = tags.replace(' ', '').split(',')
-        
+
     if many is None:
         returnDict.update({'many': 'This field cannot be empty'})
     else:
         many = formdata_bool(many)
-        
+
     if exact is None:
         returnDict.update({'exact': 'This field cannot be empty'})
     else:
         exact = formdata_bool(exact)
-        
+
     # Return if at least one is missing
     if bool(returnDict):
         return Response(
@@ -198,7 +203,7 @@ def delete_course(request):
             },
             status=status.HTTP_400_BAD_REQUEST
         )
-        
+
     # Validate boolean values
     if many is None or exact is None:
         return Response(
@@ -208,13 +213,13 @@ def delete_course(request):
             },
             status=status.HTTP_400_BAD_REQUEST
         )
-    
+
     # Get filter by exact tags or not
     if exact:
         filter_name = {'tags__name': tags}
     else:
         filter_name = {'tags__name__in': tags}
-    
+
     # Filter by tags
     course = Course.objects.filter(**filter_name)
     if not course:
@@ -225,39 +230,41 @@ def delete_course(request):
             },
             status=status.HTTP_204_NO_CONTENT
         )
-    
+
     # Check if delete many or not
     if not many:
         course = course.first()
         names = course.name
     else:
         names = course.values_list('name', flat=True)
-        
+
     # Delete
     course.delete()
-    
+
     return Response(
-            data={
-                'details': 'Ok',
-                'deleted_names': names
-            },
-            status=status.HTTP_202_ACCEPTED
+        data={
+            'details': 'Ok',
+            'deleted_names': names
+        },
+        status=status.HTTP_202_ACCEPTED
     )
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def get_tags(request):
     """
         Take in limit (optional)
-        
+
         Get all tags in db sorted in most frequently used. If limit is specified, only return 'limit' number of tags.
     """
     limit = request.GET.get('limit')
     if limit is None:
         tags = Course.tags.most_common().values('name', 'num_times')
     else:
-        tags = Course.tags.most_common()[:int(limit)].values('name', 'num_times')
-    
+        tags = Course.tags.most_common()[:int(
+            limit)].values('name', 'num_times')
+
     return Response(
         data={
             'details': 'Ok',
@@ -266,12 +273,13 @@ def get_tags(request):
         status=status.HTTP_200_OK
     )
 
+
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def recommend_tags(request):
     """
         Take in txt.
-        
+
         Return all tags containing txt as substring. If txt is empty return empty list.
     """
     txt = request.GET.get('txt')
@@ -280,17 +288,18 @@ def recommend_tags(request):
             data=[],
             status=status.HTTP_200_OK
         )
-    
+
     tag_names = Course.objects.all().values_list('tags__name', flat=True).distinct()
     returnList = []
     for name in tag_names:
         if txt in name:
             returnList.append(name)
-    
+
     return Response(
         data=returnList,
         status=status.HTTP_200_OK
     )
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -303,7 +312,7 @@ def list_course(request):
         Param tags must be in the form of: tag1, tag2, tag3 (whitespace is optional)
     """
     tags = request.GET.get('tags')
-    
+
     if tags is None:
         course = Course.objects.all()
     else:
@@ -314,9 +323,9 @@ def list_course(request):
         else:
             filter_name = {'tags__name__in': tags}
         course = Course.objects.filter(**filter_name).distinct()
-    
+
     data = CourseSerializer(course, many=True).data
-    
+
     return Response(
         data=data,
         status=status.HTTP_200_OK
