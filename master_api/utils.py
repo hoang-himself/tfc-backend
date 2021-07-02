4bc4d9202d7fa090d75da51f038ea11aae741d3c
simplynormal <nguyenchinhkhoi@gmail.com>
2021-07-02 19:48:21 +0700

    Forgot this for big update

---
+++ master_api/utils.py
@@ -0,0 +1,66 @@
+from rest_framework.exceptions import NotFound, ParseError
+from django.core.exceptions import ValidationError
+
+def _get_queryset(klass):
+    """
+    Return a QuerySet or a Manager.
+    Duck typing in action: any class with a `get()` method (for
+    get_object_or_404) or a `filter()` method (for get_list_or_404) might do
+    the job.
+    """
+    # If it is a model class or anything else with ._default_manager
+    if hasattr(klass, '_default_manager'):
+        return klass._default_manager.all()
+    return klass
+
+
+def get_object_or_404(klass, name_print, *args, **kwargs):
+    """
+    Use get() to return an object, or raise a Http404 exception if the object
+    does not exist.
+
+    klass may be a Model, Manager, or QuerySet object. All other passed
+    arguments and keyword arguments are used in the get() query.
+
+    Like with QuerySet.get(), MultipleObjectsReturned is raised if more than
+    one object is found.
+    """
+    queryset = _get_queryset(klass)
+    if not hasattr(queryset, 'get'):
+        klass__name = klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
+        raise ValueError(
+            "First argument to get_object_or_404() must be a Model, Manager, "
+            "or QuerySet, not '%s'." % klass__name
+        )
+    try:
+        return queryset.get(*args, **kwargs)
+    except queryset.model.DoesNotExist:
+        raise NotFound(f'{name_print} does not exist')
+    
+def model_full_clean(klass):
+    try:
+        klass.full_clean()
+    except ValidationError as message:
+        raise ParseError(dict(message))
+    
+
+
+def get_list_or_404(klass, name_print, *args, **kwargs):
+    """
+    Use filter() to return a list of objects, or raise a Http404 exception if
+    the list is empty.
+
+    klass may be a Model, Manager, or QuerySet object. All other passed
+    arguments and keyword arguments are used in the filter() query.
+    """
+    queryset = _get_queryset(klass)
+    if not hasattr(queryset, 'filter'):
+        klass__name = klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
+        raise ValueError(
+            "First argument to get_list_or_404() must be a Model, Manager, or "
+            "QuerySet, not '%s'." % klass__name
+        )
+    obj_list = list(queryset.filter(*args, **kwargs))
+    if not obj_list:
+        raise NotFound(f'{name_print} does not exist')
+    return obj_list
