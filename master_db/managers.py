d24379041ecec7b5439ecf04bb38ece607c641ca
Hoang NGUYEN <baauco@gmail.com>
2021-07-06 15:52:03 +0700

    This file need not be changed

--- master_db/managers.py
+++ master_db/managers.py
@@ -2,21 +2,16 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, first_name, mid_name, last_name, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('Email must be set')
-        if not first_name:
-            raise ValueError('First name must be set')
-        if not last_name:
-            raise ValueError('Last name must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name,
-                          mid_name=mid_name, last_name=last_name, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, first_name, mid_name, last_name, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -25,5 +20,4 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email=email, password=password, first_name=first_name,
-                                mid_name=mid_name, last_name=last_name, **extra_fields)
+        return self.create_user(email=email, password=password, **extra_fields)
