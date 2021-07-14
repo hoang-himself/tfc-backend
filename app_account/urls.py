86b3ca0bd158b06a783fe94c34a705e79f0f8a09
Hoang NGUYEN <baauco@gmail.com>
2021-07-14 23:38:24 +0700

    Updated endpoint tagname

--- app_account/urls.py
+++ app_account/urls.py
@@ -4,15 +4,15 @@ from . import views
 app_name = 'app_account'
 
 urlpatterns = [
-    path('get', views.get_self, name='get_self'),
+    path('get', views.get_self, name='get-self'),
 
-    path('list', views.list_user, name='list_user'),
-    path('create', views.create_user, name='create_user'),
-    path('edit', views.edit_user, name='edit_user'),
-    path('delete', views.delete_user, name='delete_user'),
+    path('list', views.list_user, name='list-user'),
+    path('create', views.create_user, name='create-user'),
+    path('edit', views.edit_user, name='edit-user'),
+    path('delete', views.delete_user, name='delete-user'),
 
-    path('staff/list', views.list_staff, name='list_staff'),
-    path('staff/create', views.create_staff, name='create_staff'),
-    path('staff/edit', views.edit_staff, name='edit_staff'),
-    path('staff/delete', views.delete_staff, name='delete_staff'),
+    path('staff/list', views.list_staff, name='list-staff'),
+    path('staff/create', views.create_staff, name='create-staff'),
+    path('staff/edit', views.edit_staff, name='edit-staff'),
+    path('staff/delete', views.delete_staff, name='delete-staff'),
 ]
