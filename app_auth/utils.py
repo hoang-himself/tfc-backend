d5743e289282b92baf939b6de44ae0fc57a2ffa3
Hoang NGUYEN <baauco@gmail.com>
2021-07-28 10:58:41 +0700

    Intermediate switch to CBV

--- app_auth/utils.py
+++ app_auth/utils.py
@@ -3,45 +3,58 @@ from annoying.functions import get_object_or_None
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-from rest_framework.exceptions import APIException
+from rest_framework import exceptions
 
 import jwt
 
 
 CustomUser = get_user_model()
+JWT_KEY = settings.JWT_KEY
 
 
-class BadAccessToken(APIException):
+def request_to_jwt(request):
+    authorization_header = request.headers.get('Authorization')
+
+    try:
+        access_token = authorization_header.split(' ')[1]
+        jwt.decode(access_token, JWT_KEY, algorithms=['HS256'])
+    except jwt.ExpiredSignatureError:
+        raise exceptions.AuthenticationFailed('Access token expired')
+    except IndexError:
+        raise exceptions.AuthenticationFailed('Token prefix missing')
+
+
+class BadAccessToken(exceptions.APIException):
     status_code = 400
     default_detail = 'Invalid access token'
     default_code = 'bad_access_token'
 
 
-class BadRefreshToken(APIException):
+class BadRefreshToken(exceptions.APIException):
     status_code = 400
     default_detail = 'Invalid refresh token'
     default_code = 'bad_refresh_token'
 
 
-class NotLoggedIn(APIException):
+class NotLoggedIn(exceptions.APIException):
     status_code = 401
     default_detail = 'User not logged in.'
     default_code = 'not_logged_in'
 
 
-class SessionExpired(APIException):
+class SessionExpired(exceptions.APIException):
     status_code = 401
     default_detail = 'Session expired.'
     default_code = 'session_expired'
 
 
-class UserNotFound(APIException):
+class UserNotFound(exceptions.APIException):
     status_code = 404
     default_detail = 'User not found.'
     default_code = 'user_not_found'
 
 
-class UserInactive(APIException):
+class UserInactive(exceptions.APIException):
     status_code = 404
     default_detail = 'User inactive.'
     default_code = 'user_inactive'
