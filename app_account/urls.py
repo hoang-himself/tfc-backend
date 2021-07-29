06a75d024c9f2eac4b832bdd55711456ccbff2bc
Hoang NGUYEN <baauco@gmail.com>
2021-07-29 23:09:29 +0700

    Renewed account

--- app_account/urls.py
+++ app_account/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
-from . import views
+
+from .views import (SelfView, UserView, StaffView)
 
 app_name = 'app_account'
 
 urlpatterns = [
-    path('get', views.get_self, name='get-user'),
+    path('me', SelfView.as_view(), name='get-self'),
 
-    path('list', views.list_user, name='list-user'),
-    path('create', views.create_user, name='create-user'),
-    path('edit', views.edit_user, name='edit-user'),
-    path('delete', views.delete_user, name='delete-user'),
+    path('list', UserView.as_view(), name='list-user'),
+    path('create', UserView.as_view(), name='create-user'),
+    path('edit', UserView.as_view(), name='edit-user'),
+    path('delete', UserView.as_view(), name='delete-user'),
 
-    path('staff/list', views.list_staff, name='list-staff'),
-    path('staff/create', views.create_staff, name='create-staff'),
-    path('staff/edit', views.edit_staff, name='edit-staff'),
-    path('staff/delete', views.delete_staff, name='delete-staff'),
+    path('staff/list', StaffView.as_view(), name='list-staff'),
+    path('staff/create', StaffView.as_view(), name='create-staff'),
+    path('staff/edit', StaffView.as_view(), name='edit-staff'),
+    path('staff/delete', StaffView.as_view(), name='delete-staff'),
 ]
--- app_account/views.py
+++ app_account/views.py
@@ -1,138 +1,103 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
-from django.core.exceptions import ValidationError
-from django.views.decorators.csrf import csrf_protect
 
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from app_auth.utils import request_to_userobj
+from app_auth.utils import request_header_to_userobj
+from master_api.views import (
+    create_object, get_object,
+    edit_object, delete_object
+)
 from master_db.serializers import CustomUserSerializer
-from master_api.views import (create_object, edit_object,
-                              delete_object, get_object)
-
-import datetime
-import re
 
 CustomUser = get_user_model()
 
 
-@api_view(['GET'])
-@permission_classes([AllowAny])
-@csrf_protect
-def get_self(request):
-    user = request_to_userobj(request)
-    return Response(CustomUserSerializer(user).data)
+class SelfView(APIView):
+    def get(self, request):
+        user = request_header_to_userobj(request)
+        return Response(CustomUserSerializer(user).data)
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-@csrf_protect
-def create_user(request):
-    return create_object(CustomUser, data=request.data)
+class UserView(APIView):
+    def post(self, request):
+        return create_object(CustomUser, data=request.data)
+
+    def get(self, request):
+        return Response(
+            CustomUserSerializer(
+                CustomUser.objects.all(), many=True).data
+        )
+
+    def patch(self, request):
+        return edit_object(CustomUser, data=request.data)
+
+    def delete(self, request):
+        return delete_object(CustomUser, data=request.data)
 
 
-@api_view(['PATCH'])
-@permission_classes([AllowAny])
-@csrf_protect
-def edit_user(request):
-    return edit_object(CustomUser, data=request.data)
+class StaffView(APIView):
+    def post(self, request):
+        """
+            Return list of users with a specified view
+        """
+        # TODO
+        filter_query = request.GET.getlist('filter')
 
+        if not filter_query:
+            filter_query = [
+                'uuid',
+                'email',
+                'first_name',
+                'last_name',
+                'birth_date',
+                'mobile',
+                'male',
+                'address',
+                'is_active',
+                'last_login',
+                'date_joined',
+                'date_updated',
+            ]
 
-@api_view(['DELETE'])
-@permission_classes([AllowAny])
-@csrf_protect
-def delete_user(request):
-    return delete_object(CustomUser, data=request.data)
+        filter_dict = {
+            'uuid': True,
+            'email': True,
+            'first_name': True,
+            'last_name': True,
+            'birth_date': True,
+            'mobile': True,
+            'male': True,
+            'address': True,
+            'avatar': False,
+            'date_joined': True,
+            # 'role__name': True,
+            'is_active': True,
+            'last_login': True,
+            'date_joined': True,
+            'date_updated': True,
+        }
 
+        # TODO
+        listZ = []
+        for key in filter_query:  # Query filter for choosing views
+            if filter_dict[key]:
+                listZ.append(key)
 
-@api_view(['GET'])
-@permission_classes([AllowAny])
-@csrf_protect
-def list_user(request):
-    return Response(
-        CustomUserSerializer(
-            CustomUser.objects.all(), many=True).data
-    )
+        # Asterisk expands list into separated args
+        # https://docs.python.org/2/tutorial/controlflow.html#unpacking-argument-lists
+        data = CustomUser.objects.all().values(*listZ)
+        return Response(data)
 
+    def get(self, request):
+        # TODO
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
 
-@api_view(['GET'])
-@permission_classes([AllowAny])
-@csrf_protect
-def list_staff(request):
-    # TODO
-    pass
+    def patch(self, request):
+        # TODO
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
 
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-@csrf_protect
-def create_staff(request):
-    """
-        Return list of users with a specified view
-    """
-    # TODO
-    filter_query = request.GET.getlist('filter')
-
-    if not filter_query:
-        filter_query = [
-            'uuid',
-            'email',
-            'first_name',
-            'last_name',
-            'birth_date',
-            'mobile',
-            'male',
-            'address',
-            'is_active',
-            'last_login',
-            'date_joined',
-            'date_updated',
-        ]
-
-    filter_dict = {
-        'uuid': True,
-        'email': True,
-        'first_name': True,
-        'last_name': True,
-        'birth_date': True,
-        'mobile': True,
-        'male': True,
-        'address': True,
-        'avatar': False,
-        'date_joined': True,
-        # 'role__name': True,
-        'is_active': True,
-        'last_login': True,
-        'date_joined': True,
-        'date_updated': True,
-    }
-
-    # TODO
-    listZ = []
-    for key in filter_query:  # Query filter for choosing views
-        if filter_dict[key]:
-            listZ.append(key)
-
-    # Asterisk expands list into separated args
-    # https://docs.python.org/2/tutorial/controlflow.html#unpacking-argument-lists
-    data = CustomUser.objects.all().values(*listZ)
-    return Response(data)
-
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-@csrf_protect
-def edit_staff(request):
-    # TODO
-    pass
-
-
-@api_view(['DELETE'])
-@permission_classes([AllowAny])
-@csrf_protect
-def delete_staff(request):
-    # TODO
-    pass
+    def delete(self, request):
+        # TODO
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
